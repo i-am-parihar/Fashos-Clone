2a3545b0653c902829c1b52712c02fc8c5762d0c
i-am-parihar <paritoshparihar4@gmail.com>
2022-05-22 07:05:40 +0530

    client - server update

--- server/src/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose") ;
-
-module.exports = () => {
-    return mongoose.connect("mongodb+srv://clone123:clone123@cluster0.xdiz7.mongodb.net/?retryWrites=true&w=majority")
-}
